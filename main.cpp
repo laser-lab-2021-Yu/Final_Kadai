a0049d0e472d3924b69ba7fcaa6124a9f955b4e5
tabosann <kimyuki0731@icloud.com>
2021-09-11 16:06:32 +0900

    update

--- main.cpp
+++ main.cpp
@@ -4,27 +4,29 @@ using namespace wfl;
 using namespace psl;
 
 static void Coding(WaveField& sub);
-static void LoadAndSave(const char* fname, WaveField& wf, const WFL_RECT& rect);
+static void LoadAndSave(const char* name, WaveField& wf, const WFL_RECT& rect);
 
-static const double gRatio = 1.0 / 8.0;
-static const Point	gRefPos(0.0, -1.0e-3, -40e-3);
-static const Point	gEyePos(0, 0.0, 20e-3);
+static const double gRatio = 1.0 / 8.0;				 // ここでスケール倍率を決める
+static const Point	gRefPos(0.0, -50.0e-3, -200e-3); // ここで参照光の位置を決める
+static const Point	gEyePos(20e-3, 0.0, 200e-3);	 // ここで視点の位置を決める
 
 int main()
 {
 	Start();    wfl::SetNumThreads();								//最大限にプロセッサコアを用いる
 
 	// モデルの設定
-	double objectSize = 3e-3 * gRatio;								//物体の実サイズ
-	Point  objectPos(0.0, 0.0, -20e-3); objectPos *= gRatio;		//物体の中心位置
+	double objectSize = 20e-3 * gRatio;								//物体の実サイズ
+	Point  objectPos(0.0, 0.0, -150e-3); objectPos *= gRatio;		//物体の中心位置
 
 	// 物体モデルファイルの読み込みと設定
 	IndexedFaceSet model;
-	model.LoadMqo("model\\pondelion.mqo");
-	model.Localize();												//物体を一時的に原点に置く
-	model.SetWidth(objectSize);										//物体サイズ(幅)を設定
-	model += objectPos;												//物体位置を設定
-	model.AutoNormalVector();										//グーローシェーディングのための準備
+	{
+		model.LoadMqo("model\\pondelion.mqo");
+		model.Localize();											//物体を一時的に原点に置く
+		model.SetWidth(objectSize);									//物体サイズ(幅)を設定
+		model += objectPos;											//物体位置を設定
+		model.AutoNormalVector();									//グーローシェーディングのための準備
+	}
 
 	// フレームバッファの設定
 	double	  rgbLambda[] = { 630e-9, 540e-9, 460e-9 };				//フルカラー用に用いる波長(R, G, B)
@@ -42,14 +44,15 @@ int main()
 
 	// 結像再生の設定
 	ImagingViewer view;
-	Point eyePos = gEyePos; eyePos *= gRatio; //視点位置
-	Point focusPos = objectPos;
-	view.SetOrigin(eyePos);
-	view.SetImagingDistance(24e-3);
-	view.SetPupilDiameter(6e-3);
-	view.SetPx(px);
-	view.SetPy(py);
-	view.Init();
+	{
+		Point eyePos = gEyePos; eyePos *= gRatio; //視点位置
+		view.SetOrigin(eyePos);
+		view.SetImagingDistance(24e-3);
+		view.SetPupilDiameter(6e-3);
+		view.SetPx(px);
+		view.SetPy(py);
+		view.Init();
+	}
 
 	// 結像再生像の作成に用いる
 	ColorImage image(view.GetNx(), view.GetNy());
@@ -67,16 +70,18 @@ int main()
 
 		// SurfaceBuilderの設定
 		SurfaceBuilder sb(frame);									//帯域制限のため，サンプル数とサンプル間隔等を正しく設定
-		sb.SetBandLimitMethod(3);									//帯域制限をレベル3にする．
-		sb.SetCenter(Point(0, 0, 0));								//帯域制限のため，ホログラムの中心位置(0,0,0)を設定
-		sb.SetDiffractionRatio(0.9);								//回折率設定
-		sb.SetCullingRate(0.6);										//カリング率設定
-		sb.SetShader(shading);										//上で用意したシェーダーオブジェクトを組み込む
+		{
+			sb.SetBandLimitMethod(3);								//帯域制限をレベル3にする．
+			sb.SetCenter(Point(0, 0, 0));							//帯域制限のため，ホログラムの中心位置(0,0,0)を設定
+			sb.SetDiffractionRatio(0.9);							//回折率設定
+			sb.SetCullingRate(0.6);									//カリング率設定
+			sb.SetShader(shading);									//上で用意したシェーダーオブジェクトを組み込む
+		}
 
 		// 物体モデルmodelからの光波を計算してフレームバッファに加算
 		sb.AddObjectFieldSb(frame, model, 1);						//スイッチバック法で物体光波計算
 
-		// 物体光波分布
+		// 物体面の光波分布
 		switch (RGBcounter)
 		{
 		case RED:	frame.SaveAsWf("output\\object-R.wf"); break;
@@ -85,7 +90,7 @@ int main()
 		}
 
 		// フレネルホログラム用の物体光波を得るためホログラムの位置(z = 0)まで伝搬計算する
-		frame.ExactAsmProp(-frame.GetOrigin().GetZ());
+		frame.AsmProp(-frame.GetOrigin().GetZ());
 
 		// ホログラム面(z = 0)で得られる物体光波の保存
 		switch (RGBcounter)
@@ -113,32 +118,38 @@ int main()
 		case BLUE:	frame.SaveAsWf("output\\fringe-B.wf"); break;
 		}
 
+		//-------------------------------------------------------
+		// ここまでが干渉縞の記録
+		// ここからが結像再生のシミュレーション
+		//-------------------------------------------------------
+
 		// 干渉縞に再生照明光を当てる
 		ref.ConvToConjugate();
 		frame *= ref;
 
 		// RGB各色の結像再生像を作成
 		view.Clear();
-		//view.SetWavelength(frame.GetWavelength());
-		view.View(frame, focusPos);
+		view.View(frame, objectPos);
 		view.ImageRotation(2);
 
 		// RGB各色の結像再生像を加算
 		image += view;
 	}
 
-	WaveField red, green, blue;
-	WFL_RECT rect(0, view.GetNy() - 1, view.GetNx() - 1, view.GetNy() / 2);
-	// 物体光波のシミュレーション結果
-	LoadAndSave("output\\object-R", red, rect);
-	LoadAndSave("output\\object-G", green, rect);
-	LoadAndSave("output\\object-B", blue, rect);
+	// 物体光波、干渉縞の画像化をする際の設定
+	double	 wDiv2 = view.GetWidth() / 2, hDiv2 = view.GetHeight() / 2;
+	WFL_RECT rect(-wDiv2, +hDiv2, +wDiv2, -hDiv2);
+
+	// 物体光のシミュレーション結果
+	LoadAndSave("output\\object-R", frame, rect);
+	LoadAndSave("output\\object-G", frame, rect);
+	LoadAndSave("output\\object-B", frame, rect);
 	// 干渉縞のシミュレーション結果
-	LoadAndSave("output\\fringe-R", red, rect);
-	LoadAndSave("output\\fringe-G", green, rect);
-	LoadAndSave("output\\fringe-B", blue, rect);
+	LoadAndSave("output\\fringe-R", frame, rect);
+	LoadAndSave("output\\fringe-G", frame, rect);
+	LoadAndSave("output\\fringe-B", frame, rect);
 	// 結像再生像のシミュレーション結果
-	image.NormalizeXYZ(10);
+	image.NormalizeXYZ();
 	image.SaveAsBmpSRGB("output\\result.bmp");
 }
 
@@ -159,12 +170,15 @@ void Coding(WaveField& fringe)
 	}
 }
 
-void LoadAndSave(const char* fname, WaveField& wf, const WFL_RECT& rect)
+void LoadAndSave(const char* name, WaveField& wf, const WFL_RECT& rect)
 {
+	wf.Clear();
+	wf.SetWindowMax();
+
 	char fullname[256];
-	sprintf_s(fullname, "%s.wf", fname);
+	sprintf_s(fullname, "%s.wf", name);
 	wf.LoadWf(fullname);
-	//wf.SetWindow(rect);
-	sprintf_s(fullname, "%s.bmp", fname);
+	wf.SetWindow(rect);
+	sprintf_s(fullname, "%s.bmp", name);
 	wf.SaveAsBmp(fullname, AMPLITUDE);
 }

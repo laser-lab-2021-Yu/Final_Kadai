a2809fb729bbab28bb7f171f73a507bedd372acd
tabosann <kimyuki0731@icloud.com>
2021-09-17 02:25:15 +0900

    update

--- main.cpp
+++ main.cpp
@@ -3,20 +3,20 @@
 using namespace wfl;
 using namespace psl;
 
-static void Coding(WaveField& sub);
-static void LoadWfAndSaveAsBmp(WaveField& wf, const WFL_RECT& rect, const char* name);
-static void LoadWfAndSaveAsBmp(WaveField& wf, const char* name);
-static const char* Str(const char* format, ...);
+static void			Coding(WaveField& sub);
+static const char*	Str(const char* format, ...);
 
-static const double gRatio = 1.0;				 // ここでスケール倍率を決める
-static const Point	gRefPos(0.0, -50.0e-3, -200e-3); // ここで参照光の位置を決める
+static const double gScale = 1.0;				 // ここでスケール倍率を決める
+static const Point	gRefPos(0.0, -40.0e-3, -200e-3); // ここで参照光の位置を決める
+
+// ここで視点の位置を決める
 static const Point	gEyePos[5] =
 { 
 	Point(0, 0, 200e-3),		// 正面
-	Point(0, 20e-3, 200e-3),	// 上
-	Point(0, -20e-3, 200e-3),	// 下
-	Point(-20e-3, 0, 200e-3),	// 左
-	Point(20e-3, 0, 200e-3)		// 右
+	Point(0, 40e-3, 200e-3),	// 上
+	Point(0, -40e-3, 200e-3),	// 下
+	Point(-40e-3, 0, 200e-3),	// 左
+	Point(40e-3, 0, 200e-3)		// 右
 };
 
 int main()
@@ -24,8 +24,8 @@ int main()
 	Start();    wfl::SetNumThreads();								//最大限にプロセッサコアを用いる
 
 	// モデルの設定
-	double objectSize = 20e-3 * gRatio;								//物体の実サイズ
-	Point  objectPos(0.0, 0.0, -150e-3); objectPos *= gRatio;		//物体の中心位置
+	double objectSize = 20e-3 * gScale;								//物体の実サイズ
+	Point  objectPos(0.0, 0.0, -150e-3); objectPos *= gScale;		//物体の中心位置
 
 	// 物体モデルファイルの読み込みと設定
 	IndexedFaceSet model;
@@ -40,7 +40,7 @@ int main()
 	// フレームバッファの設定
 	double	  rgbLambda[] = { 630e-9, 540e-9, 460e-9 };				//フルカラー用に用いる波長(R, G, B)
 	double	  px = 0.8e-6,				py = 0.8e-6;				//サンプリング間隔
-	int		  nx = 64 * 1024 * gRatio,  ny = 64 * 1024 * gRatio;	//サンプリング数
+	int		  nx = 64 * 1024 * gScale,  ny = 64 * 1024 * gScale;	//サンプリング数
 	WaveField frame(nx, ny, px, py);
 
 	// シェーダーの設定
@@ -49,16 +49,18 @@ int main()
 
 	// 参照光の設定
 	WaveField ref(nx, ny, px, py);
-	Point	  refPos = gRefPos; refPos *= gRatio;
+	Point	  refPos = gRefPos; refPos *= gScale;
 
 	// 正面、上下左右の各視点を計算
-	size_t eyePosEnd = _countof(gEyePos);
+	int eyePosEnd = _countof(gEyePos);
 	for (int eyePosIdx = 0; eyePosIdx < eyePosEnd; ++eyePosIdx)
 	{
+		Printf("\n\n%d番目の視点の計算開始\n", eyePosIdx);
+
 		// 結像再生の設定
 		ImagingViewer view;
 		{
-			Point eyePos = gEyePos[eyePosIdx]; eyePos *= gRatio; //視点位置
+			Point eyePos = gEyePos[eyePosIdx]; eyePos *= gScale;	//視点位置
 			view.SetOrigin(eyePos);
 			view.SetImagingDistance(24e-3);
 			view.SetPupilDiameter(6e-3);
@@ -66,6 +68,9 @@ int main()
 			view.SetPy(py);
 			view.Init();
 		}
+
+		// 物体光波のシミュレーション画像の作成に用いる
+		ColorImage object(view.GetNx(), view.GetNy());
 
 		// 結像再生像の作成に用いる
 		ColorImage image(view.GetNx(), view.GetNy());
@@ -94,73 +99,44 @@ int main()
 			// 物体モデルmodelからの光波を計算してフレームバッファに加算
 			sb.AddObjectFieldSb(frame, model, 1);						//スイッチバック法で物体光波計算
 
-			// 物体面の光波分布
-			switch (RGBcounter)
-			{
-			case RED:	frame.SaveAsWf(Str("output\\%d\\object-R.wf", eyePosIdx)); break;
-			case GREEN: frame.SaveAsWf(Str("output\\%d\\object-G.wf", eyePosIdx)); break;
-			case BLUE:	frame.SaveAsWf(Str("output\\%d\\object-B.wf", eyePosIdx)); break;
-			}
+			//-----------------------------------------
+			// (1) 物体光波シミュレーション再生像を計算
+			//-----------------------------------------
+			view.SpectralView(frame, objectPos, object);
 
 			// フレネルホログラム用の物体光波を得るためホログラムの位置(z = 0)まで伝搬計算する
 			frame.AsmProp(-frame.GetOrigin().GetZ());
-
-			// ホログラム面(z = 0)で得られる物体光波の保存
-			switch (RGBcounter)
-			{
-			case RED:	frame.SaveAsWf(Str("output\\%d\\frame-R.wf", eyePosIdx)); break;
-			case GREEN: frame.SaveAsWf(Str("output\\%d\\frame-G.wf", eyePosIdx)); break;
-			case BLUE:	frame.SaveAsWf(Str("output\\%d\\frame-B.wf", eyePosIdx)); break;
-			}
 
 			// 球面波を参照光として設定
 			ref.Clear();
 			ref.SetWavelength(rgbLambda[RGBcounter - 1]);
 			ref.AddSphericalWave(refPos);
-			ref.ConvToConjugate();
 
 			// 二値化干渉縞の作成
-			frame *= ref;
+			frame *= ref.ConvToConjugate();
 			Coding(frame);
-
-			// 干渉縞の保存
-			switch (RGBcounter)
-			{
-			case RED:	frame.SaveAsWf(Str("output\\%d\\fringe-R.wf", eyePosIdx)); break;
-			case GREEN: frame.SaveAsWf(Str("output\\%d\\fringe-G.wf", eyePosIdx)); break;
-			case BLUE:	frame.SaveAsWf(Str("output\\%d\\fringe-B.wf", eyePosIdx)); break;
-			}
 
 			//-------------------------------------------------------
 			// ここまでが干渉縞の記録
-			// ここからが結像再生のシミュレーション
+			// ここからが干渉縞シミュレーション
 			//-------------------------------------------------------
 
 			// 干渉縞に再生照明光を当てる
-			ref.ConvToConjugate();
-			frame *= ref;
+			frame *= ref.ConvToConjugate();
 
-			// カラーの結像再生像を作成
+			//---------------------------------------
+			// (2) 干渉縞シミュレーション再生像を計算
+			//---------------------------------------
 			view.SpectralView(frame, objectPos, image);
 		}
 
-		// 物体光波、干渉縞の画像化をする際の設定
-		double	 wDiv2 = view.GetWidth() / 2, hDiv2 = view.GetHeight() / 2;
-		WFL_RECT rect(-wDiv2, +hDiv2, +wDiv2, -hDiv2);
+		// 物体光波シミュレーション再生像を保存
+		object.NormalizeXYZ();
+		object.SaveAsBmpSRGB(Str("output\\object%d.bmp", eyePosIdx));
 
-		// 物体光のシミュレーション結果
-		LoadWfAndSaveAsBmp(frame, Str("output\\%d\\object-R", eyePosIdx));
-		LoadWfAndSaveAsBmp(frame, Str("output\\%d\\object-G", eyePosIdx));
-		LoadWfAndSaveAsBmp(frame, Str("output\\%d\\object-B", eyePosIdx));
-
-		// 干渉縞のシミュレーション結果
-		LoadWfAndSaveAsBmp(frame, Str("output\\%d\\fringe-R", eyePosIdx));
-		LoadWfAndSaveAsBmp(frame, Str("output\\%d\\fringe-G", eyePosIdx));
-		LoadWfAndSaveAsBmp(frame, Str("output\\%d\\fringe-B", eyePosIdx));
-
-		// 結像再生像のシミュレーション結果
+		// 干渉縞シミュレーション再生像を保存
 		image.NormalizeXYZ();
-		image.SaveAsBmpSRGB(Str("output\\%d\\result.bmp", eyePosIdx));
+		image.SaveAsBmpSRGB(Str("output\\view%d.bmp", eyePosIdx));
 	}
 }
 
@@ -179,33 +155,6 @@ void Coding(WaveField& fringe)
 			fringe.SetPixel(ix, iy, Complex(amplitude, 0.0));
 		}
 	}
-}
-
-void LoadWfAndSaveAsBmp(WaveField& wf, const WFL_RECT& rect, const char* name)
-{
-	wf.Clear();
-	wf.SetWindowMax();
-
-	char fname[256];
-	sprintf_s(fname, "%s.wf", name);
-	wf.LoadWf(fname);
-	sprintf_s(fname, "%s.bmp", name);
-	wf.Normalize();
-	wf.SetWindow(rect);
-	wf.SaveAsBmp(fname, AMPLITUDE);
-}
-
-void LoadWfAndSaveAsBmp(WaveField& wf, const char* name)
-{
-	wf.Clear();
-	wf.SetWindowMax();
-	
-	char fname[256];
-	sprintf_s(fname, "%s.wf", name);
-	wf.LoadWf(fname);
-	sprintf_s(fname, "%s.bmp", name);
-	wf.Normalize();
-	wf.SaveAsBmp(fname, AMPLITUDE);
 }
 
 #include <stdarg.h>
